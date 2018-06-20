e8b455f8e13e7e3c0bac1932b4c43b830e450616
15010071819@163.com <monan1012@163.com>
2018-06-20 15:14:29 +0800

    xiugai

--- src/helloowrld.java
+++ src/helloowrld.java
@@ -1,5 +1,11 @@
 public class helloowrld {
     public static void main(String[] args) {
         System.out.println("sss");
+        String aa="";
+        if (aa == null) {
+            System.out.println("asd");
+        }
+
+        System.out.println("asd");
     }
 }
